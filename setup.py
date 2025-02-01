454971d9f25adc5a21a125cd5e5a9fcd89d23684
alfarttusie <m.alfarttusie@gmail.com>
2025-02-01 11:39:18 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="ProxiesGrabber",
-    version="1.0.3",
+    version="1.0.4",
     author="alfarttusie",
     author_email=" muhammed@alfarttusie.com",
     description="A Python package for scraping free proxy lists from various online sources.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/alfarttusie/ProxyGrabber",
+    url="https://github.com/alfarttusie/ProxiesGrabber",
     packages=find_packages(),
     install_requires=[
         "requests"
